6ffdc69780f2a593690f805f705e80dc47b36567
shivacharan999 <charan02ch@gmail.com>
2023-12-19 09:55:31 -0800

    final_code of roboshop_shell *** Do not change***

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
   most_recent = true  
   name_regex   = "devops-practice-ansible"
-  owners = [data.aws_caller_identity.current.account_id] 
+  owners = ["973714476881"] 
   
 }
 
