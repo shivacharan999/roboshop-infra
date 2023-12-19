62f63c19c18e93ac88a2e526ff5f168fa810c331
shivacharan999 <charan02ch@gmail.com>
2023-12-19 04:37:47 -0800

    final_code of roboshop_shell *** Do not change***

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
-  most_recent = true
-  name_regex   = "Centos-8-DevOps-Practice"
-  owners = ["973714476881"]  
+  most_recent = true  
+  name_regex   = "devops-practice-ansible"
+  owners = [data.aws_caller_identity.current.account_id] 
   
 }
 
@@ -28,9 +28,7 @@ resource "null_resource" "provisioner" {
         }
 
         inline = [
-            "git clone https://github.com/shivacharan999/roboshop-shell.git",
-            "cd roboshop-shell",
-            "sudo bash ${var.component}.sh ${var.password}"
+           "ansible-pull -i localhost, -U https://github.com/shivacharan999/roboshop-ansible.git roboshop.yml -r role_mame= ${var.component}"
         ]
 }
 
