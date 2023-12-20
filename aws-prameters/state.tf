48828080b25531f67498136d30eba5cc69e57e35
shivacharan999 <charan02ch@gmail.com>
2023-12-19 22:25:34 -0800

    final_code of roboshop_shell *** Do not change***

--- aws-prameters/state.tf
+++ aws-prameters/state.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {}  
 }
 
-resource "aws_ssm_parameter" "foo" {
+resource "aws_ssm_parameter" "parameter" {
     count = length(var.parameters)
     name  =  var.parameters[count.index].name
     type  =  var.parameters[count.index].type
